ab04c83da812a45425dce7717512ce339fc336ec
Steven Sousa <silver.steven67@gmail.com>
2024-11-09 18:51:28 -0500

    Fixed ARIMA.py

--- Population-Collapse/ARIMA.py
+++ Population-Collapse/ARIMA.py
@@ -1,18 +1,8 @@
-''' Authors: Steven Sousa & Nicholas Pace
-    Instructor: Dr. Poonam Kumari
-    Course: CS470 - Intro to Artificial Intelligence
-    Institution: Bridgewater State University 
-    Date: 11/08/2024
-    version: 1.0
-    Description: This is the ARIMA file for the Population Collapse project. This file has the following objectives:
-        1. Read in the data from the .xlsx file.
-        2. Perform seasonal decomposition on user selected country and indicator.
-        3. Fit an ARIMA model to the data, make predictions, and evaluate the model.'''
-
 import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.tsa.arima.model import ARIMA
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
+import pandas as pd
+import pmdarima as pm
 import utils
 
 def plot_decomposition(decomposition, indicator):
@@ -42,14 +32,16 @@ def plot_decomposition(decomposition, indicator):
     plt.tight_layout()
     plt.show()
 
-def fit_arima_model(train_data, test_data):
-    '''Fit an ARIMA model to the training data and make predictions on the test data.'''
+def fit_autoarima_model(train_data, test_data):
+    '''Fit an ARIMA model using auto_arima to automatically select the best parameters, and make predictions on the test data.'''
 
-    model = ARIMA(train_data, order=(5, 1, 0))  # (p, d, q) - Experiment with these orders
-    model_fit = model.fit()
-    predictions = model_fit.predict(start=len(train_data), end=len(train_data) + len(test_data) - 1)
-    rmse = mean_squared_error(test_data, predictions, squared=False)
+    model = pm.auto_arima(train_data, seasonal=True, m=12, stepwise=True, trace=True)
+    print(f"Best ARIMA model order: {model.order}")
+
+    predictions = model.predict(n_periods=len(test_data))
+    rmse = root_mean_squared_error(test_data, predictions)
     print(f'RMSE: {rmse}')
+    
     return predictions
 
 def plot_predictions(test_data, predictions, country, indicator):
@@ -68,22 +60,34 @@ def main():
     population_data = utils.load_data(utils.FILE_PATH)
     if population_data is None:
         return
+    
+    # Convert Time to datetime and set as index (required for ARIMA modeling)
+    population_data['Time'] = pd.to_datetime(population_data['Time'], format='%Y')
+    population_data = population_data.set_index(['Country', 'Time'])
+    population_data = population_data[~population_data.index.duplicated(keep='first')]
 
-    unique_countries = population_data['Country'].unique()
+    # Check unique countries and get the user's selection
+    unique_countries = population_data.index.get_level_values('Country').unique()
     selected_country = utils.get_user_selection(unique_countries, "Available Countries:")
 
-    country_data = population_data[population_data["Country"] == selected_country]
-    indicators = country_data.columns[2:]  # Exclude 'Time' and 'Country'
+    # Filter data for the selected country and get available indicators
+    country_data = population_data[population_data.index.get_level_values('Country') == selected_country]
+    indicators = country_data.columns[1:]  # Exclude 'Time' and 'Country' columns
     selected_indicator = utils.get_user_selection(indicators, "\nAvailable Indicators:")
-
     indicator_data = country_data[selected_indicator]
+    indicator_data = indicator_data.loc[selected_country]
+
+    # Decompose the time series
     decomposition = seasonal_decompose(indicator_data, model='additive', period=12)
     plot_decomposition(decomposition, indicator_data)
 
+    # Split the data into training and test sets (50% split)
     split_index = len(indicator_data) // 2
     train_data = indicator_data[:split_index]
     test_data = indicator_data[split_index:]
-    predictions = fit_arima_model(train_data, test_data)
+
+    # Fit the ARIMA model using auto_arima
+    predictions = fit_autoarima_model(train_data, test_data)
     plot_predictions(test_data, predictions, selected_country, selected_indicator)
 
 if __name__ == "__main__":
